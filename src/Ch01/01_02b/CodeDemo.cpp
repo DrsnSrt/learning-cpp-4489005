8ffa6aecb312d6bf18f92aa967d1c0aaa0568258
DrsnSrt <sert.dursun@gmail.com>
2024-12-08 17:10:31 +0000

    result

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,17 @@
 // Learning C++ 
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+#include <iostream>
+#include <string>
+
+int main(){
+    std::cout << "Please enter your name" << std::endl;
+    std::string name;
+    std::cin >> name;
+    std::cout << "Nice to meet you " << name;
+
+    std::cout << std::endl << std::endl;
+
+    return 0;
+}
